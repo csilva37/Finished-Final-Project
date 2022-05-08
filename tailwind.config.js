4c76a8f2006392c7fe2c747670161b557cd9649b
csilva37 <csilva37@stu.southtexascollege.edu>
2022-05-08 16:25:00 -0500

    Fixed Formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      minHeight: {
-        "screen-nonav": "calc(100vh - 88px)",
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
